fb40b6622cf477ee27c026daa9e672c9bf49fa0e
Paantee <teemu-paananen@hotmail.com>
2022-12-05 18:23:44 +0200

    Customer CRUD

--- src/components/Customerlist.js
+++ src/components/Customerlist.js
@@ -10,13 +10,16 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import AddIcon from '@mui/icons-material/Add';
+import Addcustomer from "./Addcustomer";
+import Editcustomer from "./Editcustomer";
 
 
 export default function Customerlist() {
 
     const gridRef = useRef();
-    const [cars, setCars] = useState([]);
+    const [customer, setCustomers] = useState([]);
     const [open, setOpen] = useState(false);
+
 
     useEffect(() => fetchData(), []);
 
@@ -57,8 +60,20 @@ export default function Customerlist() {
             filter: true
         },
         {
-            field: 'links.href',
-            
+            sortable: false,
+            filter: false,
+            width: 100, 
+            valueGetter: (row) => row.data.links[0].href,
+            cellRenderer: row => <Editcustomer updateCustomer={updateCustomer} customer={row.data} url={row.value}/>
+        },
+        {
+            sortable: false,
+            filter: false,
+            width: 120,
+            headerName: '',
+            field: 'link',
+            valueGetter: (row) => row.data.links[0].href,
+            cellRenderer: row => <Button color = 'error' startIcon={<DeleteIcon />} onClick={() => deleteCustomer(row.value)}>Delete</Button>
         }
         
     ]
@@ -66,12 +81,43 @@ export default function Customerlist() {
     const fetchData = () => {
         fetch('https://customerrest.herokuapp.com/api/customers')
         .then(response => response.json())
-        .then(data => setCars(data.content))
+        .then(data => setCustomers(data.content))
         console.log("customers")
     }
 
 
+    const deleteCustomer = (link) => {
+        if (window.confirm('Are you sure?')) {
+            fetch(link, {method: 'DELETE'})
+            .then(res => fetchData())
+            .catch(err => console.error(err))
+        }
+      }
     
+      const saveCustomer = (customer) => {
+        fetch('https://customerrest.herokuapp.com/api/customers', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(customer)
+        })
+        .then(res => fetchData())
+        .catch(err => console.error(err))
+      }
+
+      const updateCustomer = (customer, link) => {
+        fetch(link, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(customer)
+        })
+        .then(res => fetchData())
+        .catch(err => console.error(err))
+      }
+
     return (
       <div>
         <nav>
@@ -82,13 +128,16 @@ export default function Customerlist() {
             <li>
               <Link to="/traininglist">Training List</Link>
             </li>
+            <li>
+            <Addcustomer saveCustomer={saveCustomer}/>
+            </li>
           </ul>
         </nav>
         <h1>Customer List</h1>
         <div className="ag-theme-material"style={{height: '900px', width: '90%', margin: 'auto'}} >
             <AgGridReact className='grid'
             columnDefs={columns}
-            rowData={cars}
+            rowData={customer}
             animateRows={true}
             ref={gridRef}
             onGridReady={params => gridRef.current = params.api}
